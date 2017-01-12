cdb45e5c8ad54b1ba8502e228371d562161771eb
BaliCoffee <duoduosir@foxmail.com>
2017-01-12 17:02:36 +0800

    在github上的提交
    Test

--- src/Test2.java
+++ src/Test2.java
@@ -3,6 +3,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("年后");
 		System.out.println("这是在github上添加的代码");
+		System.out.println("这是在github上添加的代码");
+		
 	}
 
 }
